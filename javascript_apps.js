6ffd805ca2a1f7174be5ad6b80c4639fce88e0ee
Sreedhar K <55496113+sreegithub19@users.noreply.github.com>
2021-06-20 23:15:08 +0530

    Update javascript_apps.js

--- javascript_apps.js
+++ javascript_apps.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const open = require('open');
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -16,10 +17,21 @@ app.get("/", (req, res) => {
         <li><a href=\'http://localhost:3000/tic_tac_toe\'>Tic-tac-toe</a></li>\
         <li><a href=\'http://localhost:3000/clock\'>Analogue clock</a></li>\
         <li><a href=\'http://localhost:3000/hangman\'>Hangman</a></li>\
+        <li><a href=\'http://localhost:3000/puzzle/index.html\'>Puzzle</a></li>\
+        <li><a href=\'http://localhost:3000/sudoku/index.html\'>Sudoku</a></li>\
+        <li><a href=\'http://localhost:3000/virtual_keyboard\'>Virtual keyboard</a></li>\
            \
    </ul> </div> \
        </div> </body> </html>'
   );
+});
+
+app.get("/sudoku/index.html",(req,res)=>{
+    res.send(`sudoku/index.html`);
+});
+
+app.get("/puzzle/index.html",(req,res)=>{
+    res.send(`puzzle/index.html`);
 });
 
 app.get("/calculator", (req, res) => {
@@ -1503,4 +1515,221 @@ startGame()
 </html>
     `);
 });
+
+app.get("/virtual_keyboard",(req,res)=>{
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Virtual Keyboard ⌨️ </title>
+        <style>
+            body * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    
+    
+    .keyboard-base{
+        max-width: 1085px;
+        padding: 20px;
+        position: absolute;
+        top: 30%;
+        background-color: rgb(252, 185, 185);
+        border-radius: 10px;
+        justify-content: center;
+    
+    
+    }
+    .section-a{
+        display: flex;
+    }
+    
+    .key {
+        background-color: rgb(243, 243, 243);
+        border: 2px solid black;
+        border-radius: 5px;
+        
+        font-size: 20px;
+        text-align: center;
+        padding-top: 17px;
+        width:50px;
+        height:50px;
+        margin:5px;
+    }
+    
+    .key:hover{
+        background-color: rgb(247, 100, 161);
+    }
+    .space {
+        width: 100%;
+    }
+    
+    .capslock{
+        width:30%;
+    }
+    .leftshift{
+        width:20%;
+    }
+    .return {
+        width:20%;
+        
+    }
+    textarea{
+        width: 30%;
+        height: 70px;
+        font-size: 30px;
+    }
+        </style>
+    </head>
+    <body>
+        <textarea type="text" id="inputText" value="" autofocus onblur="this.focus()" onkeydown="return false" onmouseup="getCaretPositon()"></textarea>
+        
+        <div class="keyboard-base">
+        <div class="section-a">
+            <div class="key" onclick="keyboardHandler(event)">~</div>
+            <div class="key" onclick="keyboardHandler(event)">1</div>
+            <div class="key" onclick="keyboardHandler(event)">2</div>
+            <div class="key" onclick="keyboardHandler(event)">3</div>
+            <div class="key" onclick="keyboardHandler(event)">4</div>
+            <div class="key" onclick="keyboardHandler(event)">5</div>
+            <div class="key" onclick="keyboardHandler(event)">6</div>
+            <div class="key" onclick="keyboardHandler(event)">7</div>
+            <div class="key" onclick="keyboardHandler(event)">8</div>
+            <div class="key" onclick="keyboardHandler(event)">9</div>
+            <div class="key" onclick="keyboardHandler(event)">0</div>
+            <div class="key" onclick="keyboardHandler(event)">-</div>
+            <div class="key" onclick="keyboardHandler(event)">+</div>
+        </div>
+        <div class="section-a">
+            <div class="key" onclick="keyboardHandler(event)">Q</div>
+            <div class="key" onclick="keyboardHandler(event)">W</div>
+            <div class="key" onclick="keyboardHandler(event)">E</div>
+            <div class="key" onclick="keyboardHandler(event)">R</div>
+            <div class="key" onclick="keyboardHandler(event)">T</div>
+            <div class="key" onclick="keyboardHandler(event)">Y</div>
+            <div class="key" onclick="keyboardHandler(event)">U</div>
+            <div class="key" onclick="keyboardHandler(event)">I</div>
+            <div class="key" onclick="keyboardHandler(event)">O</div>
+            <div class="key" onclick="keyboardHandler(event)">P</div>
+            <div class="key" onclick="keyboardHandler(event)">[</div>
+            <div class="key" onclick="keyboardHandler(event)">]</div>
+            <div class="key backslash" onclick="keyboardHandler(event)">\</div>
+        </div>
+        <div class="section-a">
+    
+            <div class="key capslock" onclick="keyboardHandler(event)">CapsLock</div>
+            <div class="key" onclick="keyboardHandler(event)">A</div>
+            <div class="key" onclick="keyboardHandler(event)">S</div>
+            <div class="key" onclick="keyboardHandler(event)">D</div>
+            <div class="key"onclick="keyboardHandler(event)">F</div>
+            <div class="key"onclick="keyboardHandler(event)">G</div>
+            <div class="key"onclick="keyboardHandler(event)">H</div>
+            <div class="key"onclick="keyboardHandler(event)">J</div>
+            <div class="key" onclick="keyboardHandler(event)">K</div>
+            <div class="key" onclick="keyboardHandler(event)">L</div>
+            <div class="key" onclick="keyboardHandler(event)">;</div>
+            <div class="key" onclick="keyboardHandler(event)">'</div>
+            <div class="key return" onclick="keyboardHandler(event)">Back</div>
+        </div>
+        <div class="section-a">
+    
+            <div class="key leftshift" onclick="keyboardHandler(event)">Shift</div>
+            <div class="key" onclick="keyboardHandler(event)">Z</div>
+            <div class="key" onclick="keyboardHandler(event)">X</div>
+            <div class="key" onclick="keyboardHandler(event)">C</div>
+            <div class="key" onclick="keyboardHandler(event)">V</div>
+            <div class="key" onclick="keyboardHandler(event)">B</div>
+            <div class="key" onclick="keyboardHandler(event)">N</div>
+            <div class="key" onclick="keyboardHandler(event)">M</div>
+            <div class="key" onclick="keyboardHandler(event)">,</div>
+            <div class="key" onclick="keyboardHandler(event)">.</div>
+            <div class="key" onclick="keyboardHandler(event)">/</div>
+        </div>
+        <div class="section-a">
+            <div class="key space" onclick="keyboardHandler(event)">Space</div>
+            </div>
+        </div>
+    </body>
+    <script>
+        var input = document.getElementById("inputText");
+    let bool = false;
+    
+    const keyboardHandler = (event) => {
+      const caretPositon = getCaretPositon();
+      const targetValue = event.target.innerText;
+      let array = input.value.split("");
+      let a;
+    
+      switch (targetValue) {
+        case "Back":
+          console.log(caretPositon);
+          if (caretPositon == 0) {
+            input.setSelectionRange(0, 0);
+          } else {
+            array.splice(caretPositon - 1, 1);
+            a = array.join("");
+            input.value = a;
+            input.setSelectionRange(caretPositon - 1, caretPositon - 1);
+          }
+    
+          break;
+        case "CapsLock":
+          toggleCaps();
+          break;
+        case "Shift":
+          toggleShift();
+          break;
+        case "Space":
+          array.splice(caretPositon, 0, " ");
+          a = array.join("");
+          input.value = a;
+          input.setSelectionRange(caretPositon + 1, caretPositon + 1);
+    
+          break;
+        default:
+          array.splice(caretPositon, 0, event.target.innerText);
+          a = array.join("");
+          input.value = a;
+          input.setSelectionRange(caretPositon + 1, caretPositon + 1);
+    
+          if (bool) {
+            bool = false;
+            toggleCaps();
+          }
+      }
+    };
+    
+    function getCaretPositon() {
+      return input.selectionStart;
+    }
+    
+    const isUpperCase = (string) => /^[A-Z]*$/.test(string);
+    
+    const toggleCaps = () => {
+      let keyboardKeys = document.getElementsByTagName("div");
+      for (let i = 13; i < keyboardKeys.length; i++) {
+        const innerElText = keyboardKeys[i].innerText;
+        const innerTextLength = innerElText.length;
+    
+        if (innerTextLength == 1 && isUpperCase(innerElText)) {
+          keyboardKeys[i].innerText = innerElText.toLowerCase();
+        } else if (innerTextLength == 1) {
+          keyboardKeys[i].innerText = keyboardKeys[i].innerText.toUpperCase();
+        }
+      }
+    };
+    
+    const toggleShift = () => {
+      toggleCaps();
+      bool = true;
+    };
+    
+    </script>
+    </html>
+    `);
+});
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+open('http://localhost:3000');
